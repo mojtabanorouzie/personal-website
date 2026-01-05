d0146ad488c21c87b9f2c80fa0d8cfef5f440e89
Mojtaba Norouzi <mojtaba.norouzie@gmail.com>
2026-01-05 20:58:34 +0330

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center">
       <h1 className="text-4xl font-bold">
-        Hello, this is my website 👋
+        Hello, this is my website 👋 :)
       </h1>
     </main>
   );
